c1d3c518cee66530291a6b4846140a4d71fac817
diehardkamikaze <diehardkamikaze@gmail.com>
2021-02-28 19:15:50 +0300

    feat: addef towel sort implementation

--- src/index.js
+++ src/index.js
@@ -2,5 +2,29 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+	var i;
+	var j;
+	var result;
+	var flag;
+	
+	if (!matrix)
+		return [];
+	i = 0;
+	result = [];
+	while (i < matrix.length)
+	{
+		if (i % 2 != 0)
+			flag = function(j) { return (matrix[i].length - 1 - j); }
+		else
+			flag = function(j) { return (j); }
+		
+		j = 0;
+		while (j < matrix[i].length)
+		{
+			result.push(matrix[i][flag(j)]);
+			j++;
+		}
+		i++;
+	}
+  return (result);
 }
